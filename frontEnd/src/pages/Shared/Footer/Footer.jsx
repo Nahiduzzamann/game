15e2fc2c149bf22c2974b5222cd487df42cf6ba4
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-01-03 16:56:58 +0600

    ok

--- frontEnd/src/pages/Shared/Footer/Footer.jsx
+++ frontEnd/src/pages/Shared/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
+        <div className='container mx-auto bg-slate-400'>
             Footer
         </div>
     );
