ebb8e61a980db28daad2ec2c41cfff9d5a7f9f77
Sazzad Hossain <57707331+sazzad9911@users.noreply.github.com>
2024-01-03 16:49:00 +0600

    xczx

---
+++ index.ts
@@ -0,0 +1,18 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import path from "path"
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+app.use(cors())
+app.use(express.static(path.join(__dirname,"frontEnd/dist")));
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname,"frontEnd/dist/index.html"))
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
