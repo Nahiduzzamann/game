3ff5f6533bb362b5062410507af5489a34cba9b8
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-01-04 16:49:11 +0600

    ok

--- frontEnd/src/pages/Shared/Header/Header.jsx
+++ frontEnd/src/pages/Shared/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className='container mx-auto'>
+            <Link to="/" className='p-4 text-white bg-gray-500 underline'> Home</Link>
         </div>
     );
 };
