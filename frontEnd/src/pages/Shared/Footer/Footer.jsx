e97a63a73a75cadab051d412a56e3ebaf3722c8a
Md Arif Hosain <arifhosain20284@gmail.com>
2024-01-04 11:23:18 +0600

    ok

--- frontEnd/src/pages/Shared/Footer/Footer.jsx
+++ frontEnd/src/pages/Shared/Footer/Footer.jsx
@@ -2,13 +2,92 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='container mx-auto'>
-           <ul>
-            <li className='m-[100px]'>Home</li>
-            <li>About</li>
-            <li>Contact Us</li>
-           </ul>
+        <div className='container mx-auto mt-5 bg-slate-200 p-5'>
+            <div className='flex gap-24 underline underline-offset-8'>
+                <a href="#" class="font-bold py-2 rounded">Terms And Conditions</a>
+                <a href="#" class="font-bold py-2 rounded">About Us</a>
+                <a href="#" class="font-bold py-2 rounded">FAQ</a>
+                <a href="#" class="font-bold py-2 rounded">Contact Us</a>
+            </div>
+
+            <div className='grid grid-cols-3 gap-24 font-sans'>
+                <div className='pt-5'>
+                    <p className='font-bold h2 pb-4'>Bangladesh's Best Cricket Exchange</p>
+                       <p>Play with the premium cricket exchange in Bangladesh.
+                        Best odds and all games available. We provide FREE livestreaming 
+                        for ALL cricket matches. Check out our promotions for Cricket Exchange here.</p><br></br>
+
+                        <p className='font-bold h2 pb-4'>Live Casino Games & Online Casino Games in Bangladesh</p>
+                        <p>Online Casino Bangladesh BABU88 provides you with the largest suite of games that have
+                         interesting gameplays and great rewards upon winning. Popular developers such as
+                          Evolution Gaming (Evo Gaming), Super Spade Gaming, Pragmatic Play and AE Casino 
+                          are all available at BABU88. Choose BABU88 for the Best Online Casino Bangladesh Experience
+                          BABU88 is an exclusive live casino and sportsbook provider with over 1000+ games to choose from
+                          Look no further as you can find Super Spade Gaming, AE Casino and Evo Gaming and Pragmatic Play 
+                          over at BABU88! Register with only afew details and you will be able to play live casino games
+                          like Baccarat, Roulette, Blackjack and Sicbo at BABU88.</p>
+                  </div>
+
+                  <div className='pt-5'>
+                    <p className='font-bold h2 pb-4'>Sports Betting</p>
+                       <p>BABU88 has the leading sportsbook that are well known to 
+                        experience gamblers and has plenty of varieties. From sports betting, cricket
+                         exchange, cricket betting and back & lay.
+                         Looking to find out the livescore of cricket, IPL, World Cup, Big Bash League,
+                        CPL, T20, Test Cricket or ICC!
+                       Find all live scores over at BABU88 with free livestreaming. Register and open up our app, 
+                       with tons of variations to play with, you will have the best experience and mobile sportsbook at BABU88
+                      BABU88 has an exclusive sportsbook for sports related activities and all your favourite sports.
+                       With every sports game available to bet, place your winning bet with BABU88 today!
+                           </p><br></br>
+
+                        <p className='font-bold h2 pb-4'>Table Games - Spribe & Kingmaker</p>
+                        <p>Look no further as BABU88 comes with the most popular table game providers such as Spribe 
+                            & Kingmaker. Play with the most popular online casino in Bangladesh.</p>
+                            <p className='font-bold h2 pb-4'>Payments Options</p>
+                        <p>The best online casino sites offer a variety of ways to deposit 
+                            and withdraw money. This makes the games accessible to every individuals based on 
+                            their own preferences. We provide different kinds of payments choices for our customers, 
+                            including e-wallets. Deposit with e-Wallets in Bangladesh.</p>
+                  </div>
+
+                  <div className='pt-5'>
+                    <p className='font-bold h2 pb-4'>Bangladesh's Premium Cricket Exchange</p>
+                       <p>Bangladesh's only premium casino with an online betting pass. Get up to 100 FREE rewards by participating in BABU88's VIP Pass. Play 
+                        premium cricket exchange at BABU88 and get rewarded! Bet ICC, bet IPL, bet T20, bet BBL,
+                         bet CPL, bet Test Cricket with us at BABU88 and get rewarded!</p><br></br>
+
+                        <p className='font-bold h2 pb-4'>Online Gaming Providers that you should check out</p>
+                        <p>The most popular online casino site in Bangladesh that holds a rising number of best casino providers from all over the world. A wide range of casino games will be available for players to encounter and play. Try out instant deposit and withdraw cash at anytime and anywhere. Spot every online casino games in our site, such as Live Casino, Slots, Cricket Exchange,
+                             Cricket Betting, Fishing, TV and Score. Being excited to become a member to enjoy the Promos, including 
+                             welcome bonus and reload bonus in term of new member, sport-book and slot game.</p>
+                  </div>
+            </div>
+
+            <div className='grid grid-cols-3 gap-24 pt-5'>
+                <div className=''>
+                <p className='font-bold h2 pb-4'>Responsible Games</p>
+                </div>
+                <div>
+                <p className='font-bold h2 pb-4'>Certified By</p>
+                </div>
+                <div>
+                <p className='font-bold h2 pb-4'>Find Us</p>
+                </div>
+                
+            </div>
+
+            <div className='grid grid-cols-1 pt-5'>
+                <div className=''>
+                    <p className='h2 pt-5'>Copyright © 2024 Babu88.All rights are reserved.</p>
+                </div>
+                
+            </div>
+
         </div>
+
+
+
     );
 };
 
