48be6375c28ed15b50dfed1eaf3ff4d3a13bc9a1
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-01-04 16:27:10 +0600

    ok

--- frontEnd/src/pages/Info/Info.jsx
+++ frontEnd/src/pages/Info/Info.jsx
@@ -1,17 +1,38 @@
-import React from 'react';
+import React from "react";
+import Header from "../Shared/Header/Header";
+import { Link, Outlet } from "react-router-dom";
+import Footer from "../Shared/Footer/Footer";
+import ScrollToTop from "../../components/ScrollToTop";
 
 const Info = () => {
-    return (
-        <div className='container mx-auto'>
-            <div>
-            <div className='grid grid-rows-3 grid-flow-col gap-4'>
-            <div class="row-span-1 bg-red-500">01</div>
-            <div class="row-span-6 bg-sky-500">02</div>
-
-            </div>
-            </div>
+  return (
+    <div className="bg-[#EBEBEB]">
+      <Header></Header>
+      <div className="container mx-auto">
+      <div className="grid grid-cols-3 gap-4">
+        <div className="bg-white">
+          <h1 className="text-center font-bold text-xl pt-4">Other Topics</h1>
+          <div className=" mt-4 flex flex-col">
+            <Link className="ml-5 py-3 font-bold uppercase " to="/info/faq">FAQ</Link>
+            <div className="h-[1px] bg-[#EBEBEB]"></div>
+            <Link className="ml-5 py-3 font-bold uppercase " to="/info/aboutUs">About Us</Link>
+            <div className="h-[1px] bg-[#EBEBEB]"></div>
+            <Link className="ml-5 py-3 font-bold uppercase " to="/info/contactUs">Contact Us</Link>
+            <div className="h-[1px] bg-[#EBEBEB]"></div>
+            <Link className="ml-5 py-3 font-bold uppercase " to="/info/ResponsibleGaming">Responsible Gaming</Link>
+            <div className="h-[1px] bg-[#EBEBEB]"></div>
+            <Link className="ml-5 py-3 font-bold uppercase " to="/info/Terms&Conditions">Terms & Conditions</Link>
+          </div>
         </div>
-    );
+        <div className="col-span-2 bg-white h-min">
+        <Outlet></Outlet>
+        </div>
+      </div>
+      </div>
+      <Footer></Footer>
+      <ScrollToTop />
+    </div>
+  );
 };
 
 export default Info;
