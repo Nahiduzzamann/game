6b39717e8d6f8cbb355fdb054adb0ff0b83dbf66
Md Arif Hosain <arifhosain20284@gmail.com>
2024-01-04 15:31:23 +0600

    ok

--- frontEnd/src/pages/Info/Info.jsx
+++ frontEnd/src/pages/Info/Info.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 const Info = () => {
     return (
         <div className='container mx-auto'>
-            hello
+            <div>
+            <div className='grid grid-rows-3 grid-flow-col gap-4'>
+            <div class="row-span-1 bg-red-500">01</div>
+            <div class="row-span-6 bg-sky-500">02</div>
+
+            </div>
+            </div>
         </div>
     );
 };
