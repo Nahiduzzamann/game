ce7720bdac3d751d1e2a67ec028aa35f6182d3d1
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-01-09 16:05:25 +0600

    ok

---
+++ frontEnd/src/components/Spinner.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Spinner = () => {
+  return (
+    <div>
+      <Spinner
+        thickness="4px"
+        speed="0.65s"
+        emptyColor="gray.200"
+        color="blue.500"
+        size="xl"
+      />
+    </div>
+  );
+};
+
+export default Spinner;
--- frontEnd/src/pages/Home/GamesSection/GamesSection.jsx
+++ frontEnd/src/pages/Home/GamesSection/GamesSection.jsx
@@ -1,43 +1,42 @@
 import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import url from "../../../module";
+import getCategory from "../../../module/getCategory";
 const GamesSection = () => {
-    const gameCategory =localStorage.getItem("category")
-    console.log(gameCategory);
+  const [data, setData] = useState();
+  useEffect(() => {
+    const cats = async () => {
+      try {
+        const cat = await getCategory(url);
+        setData(cat.data);
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+    cats();
+    //setData(cat);
+    //console.log(cat);
+  }, []);
   return (
     <div className="px-6 py-3">
-      <Tabs isFitted variant='soft-rounded'  colorScheme='blue'>
-        <TabList mb="1em">
-          <Tab className=""><h2 className="text-xl">Cricket</h2></Tab>
-          <Tab>Casino</Tab>
-          <Tab>Slot</Tab>
-          <Tab>Table</Tab>
-          <Tab>Sports</Tab>
-          <Tab>Fishing</Tab>
-          <Tab>Crash</Tab>
+      <Tabs isFitted variant="soft-rounded" colorScheme="blue">
+        <TabList mb="1em" className="flex flex-wrap bg-white rounded-md p-2">
+          {data &&
+            data?.map((data, i) => {
+              return (
+                <Tab key={i} className="w-1/2 md:w-auto mb-2 md:mb-0">
+                  <h2 className="text-md text-black">{data?.title}</h2>
+                </Tab>
+              );
+            })}
         </TabList>
         <TabPanels>
-          <TabPanel>
-            <p>one!</p>
-          </TabPanel>
-          <TabPanel>
-            <p>two!</p>
-          </TabPanel>
-          <TabPanel>
-            <p>fdvs!</p>
-          </TabPanel>
-          <TabPanel>
-            <p>sdvfedfs!</p>
-          </TabPanel>
-          <TabPanel>
-            <p>sfedfwsa!</p>
-          </TabPanel>
-          <TabPanel>
-            <p>edfvedfswed!</p>
-          </TabPanel>
-          <TabPanel>
-            <p>egfesdgrdfe4rf!</p>
-          </TabPanel>
+          {data &&
+            data?.map((data, i) => (
+              <TabPanel key={i}>
+                <p>{i}</p>
+              </TabPanel>
+            ))}
         </TabPanels>
       </Tabs>
     </div>
