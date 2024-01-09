810f5fe98ad390cfba2a1071769550109e4a4391
Sazzad Hossain <57707331+sazzad9911@users.noreply.github.com>
2024-01-09 11:12:58 +0600

    hftg

--- frontEnd/src/pages/Info/Info.jsx
+++ frontEnd/src/pages/Info/Info.jsx
@@ -9,8 +9,8 @@ const Info = () => {
     <div className="bg-[#EBEBEB]">
       <Header></Header>
       <div className="container mx-auto">
-      <div className="grid grid-cols-3 gap-4">
-        <div className="bg-white p-5">
+      <div className="md:grid md:grid-cols-3 gap-4">
+        <div className="bg-white p-5 my-4 h-min ">
           <h1 className="text-center p-5 font-bold border-b-4 border-indigo-500 text-3xl">Other Topics</h1>
           <div className=" mt-4 flex flex-col">
             <NavLink activeClassName="active" className="ml-5 py-3 font-bold uppercase " to="/info/faq">FAQ</NavLink>
@@ -24,7 +24,7 @@ const Info = () => {
             <NavLink activeClassName="active" className="ml-5 py-3 font-bold uppercase " to="/info/Terms&Conditions">Terms & Conditions</NavLink>
           </div>
         </div>
-        <div className="col-span-2 bg-white h-min">
+        <div className="col-span-2 bg-white h-min md:mt-4">
         <Outlet></Outlet>
         </div>
       </div>
