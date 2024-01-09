1b36b490ca0df62348f22c61eedec502933e9702
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-01-09 16:54:44 +0600

    okay

--- frontEnd/src/pages/Home/GamesSection/GamesSection.jsx
+++ frontEnd/src/pages/Home/GamesSection/GamesSection.jsx
@@ -3,12 +3,15 @@ import React, { useEffect, useState } from "react";
 import url from "../../../module";
 import getCategory from "../../../module/getCategory";
 const GamesSection = () => {
-  const [data, setData] = useState();
+  const [dataCategory, setDataCategory] = useState();
+  const [dataSubCategory, setDataSubCategory] = useState();
+  // console.log(dataSubCategory);
+
   useEffect(() => {
     const cats = async () => {
       try {
         const cat = await getCategory(url);
-        setData(cat.data);
+        setDataCategory(cat.data);
       } catch (error) {
         console.error(error.message);
       }
@@ -17,24 +20,38 @@ const GamesSection = () => {
     //setData(cat);
     //console.log(cat);
   }, []);
+
+  const handleSubCategory = (index = 0) => {
+    const category = dataCategory[index];
+    const subCategory = category.subCategory;
+    setDataSubCategory(subCategory);
+  };
   return (
     <div className="px-6 py-3">
       <Tabs isFitted variant="soft-rounded" colorScheme="blue">
         <TabList mb="1em" className="flex flex-wrap bg-white rounded-md p-2">
-          {data &&
-            data?.map((data, i) => {
+          {dataCategory &&
+            dataCategory?.map((data, i) => {
               return (
-                <Tab key={i} className="w-1/2 md:w-auto mb-2 md:mb-0">
+                <Tab
+                  onClick={() => handleSubCategory(i)}
+                  key={i}
+                  className="w-1/2 md:w-auto mb-2 md:mb-0"
+                >
                   <h2 className="text-md text-black">{data?.title}</h2>
                 </Tab>
               );
             })}
         </TabList>
         <TabPanels>
-          {data &&
-            data?.map((data, i) => (
+          {dataCategory &&
+            dataCategory?.map((data, i) => (
               <TabPanel key={i}>
-                <p>{i}</p>
+                <div className="flex flex-wrap">
+                  {dataSubCategory?.map((data, i) => (
+                    <p className="p-2 bg-black text-white m-2" key={i}>{data?.name}</p>
+                  ))}
+                </div>
               </TabPanel>
             ))}
         </TabPanels>
