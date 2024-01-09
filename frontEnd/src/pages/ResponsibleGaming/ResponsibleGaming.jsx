97970b57368debdf332d38abe0feb380307652d9
Md Arif Hosain <arifhosain20284@gmail.com>
2024-01-09 10:57:08 +0600

    ok

--- frontEnd/src/pages/ResponsibleGaming/ResponsibleGaming.jsx
+++ frontEnd/src/pages/ResponsibleGaming/ResponsibleGaming.jsx
@@ -2,8 +2,40 @@ import React from 'react';
 
 const ResponsibleGaming = () => {
     return (
-        <div>
-            ResponsibleGaming
+        <div className='p-5'>
+            <h1 className='text-center p-5 font-bold border-b-4 border-indigo-500 text-3xl'>FAQ</h1>
+
+            <p className='font-bold pt-5 pb-2'>FAQ</p>
+
+            <p className='font-bold pt-5 pb-2 underline underline-offset-5'>Age Requirement</p>
+            <p className='font-medium'>BABU88's customers must be 18 years of age or older and agree to abide by the terms and conditions set by the company.</p>
+
+            <p className='font-bold pt-5 pb-2 underline underline-offset-5'>Forgot Your Username Or Password?</p>
+            <p className='font-medium'>Customers can automatically link to online customer service by clicking 'Forgot Password'.
+             The 24-hour online customer service will provide you with a solution after passing a professional verification plan.</p>
+
+          <p className='font-bold pt-5 pb-2 underline underline-offset-5'>How To Deposit</p>
+            <p className='font-medium'>There are several ways to deposit the platform: bank transfer, DhaPay (h5, secured), DhaPay (h5, secured).</p>
+
+          <p className='font-bold pt-5 pb-2 underline underline-offset-5'>Steps To Deposit</p>
+            <p className='font-medium'>1. Please click on 'deposit' at the top right of the homepage to enter the platform deposit page.<br></br>
+            2. After entering the amount you want to deposit, click 'Select Channel' on the right.<br></br>
+            3. You can click below deposit channels.<br></br>
+            (The upper and lower limits of recharge are subject to the payment channel)</p>
+
+            <p className='font-bold pt-5 pb-2 underline underline-offset-5'>How To Withdraw Money</p>
+            <p className='font-medium'>By clicking 'Withdraw' at the top right of the homepage, enter your withdrawal password and
+             the amount you want to withdraw, and then click the withdrawal after completion.<br></br>
+             For the first withdrawal, please bind the withdrawal bank card to the bank card message.<br></br>
+             Withdrawal limit is ৳800, withdrawal limit is ৳30,000 per day.
+             </p>
+
+           <p className='font-bold pt-5 pb-2 underline underline-offset-5'>Personal Data Security</p>
+            <p className='font-medium'>The company will ensure that your personal information is not disclosed to any third party.<br></br>
+            The company will also ensure the security of your personal information and ensure that your information is restricted to our company.</p>
+
+          <p className='font-bold pt-5 pb-2 underline underline-offset-5'>Central Wallet</p>
+            <p className='font-medium pb-5'>All deposited amount and bonus will be deposited into a single central wallet and bets can be placed.</p>
         </div>
     );
 };
--- frontEnd/src/pages/Terms&Conditions/Terms&Conditions.jsx
+++ frontEnd/src/pages/Terms&Conditions/Terms&Conditions.jsx
@@ -2,8 +2,65 @@ import React from 'react';
 
 const TermsConditions = () => {
     return (
-        <div>
-            Terms&Conditions
+        <div className='p-5'>
+            <h1 className='text-center p-5 font-bold border-b-4 border-indigo-500 text-3xl'>Terms & Conditions</h1>
+
+            <p className='font-bold pt-5 pb-2'>Terms & Conditions</p>
+
+            <p className='font-bold pt-5 pb-2 underline underline-offset-5'>Terms & Conditions</p>
+            <p className='font-medium'>Welcome to BABU88, Bangladesh #1 Cricket Exchange & Betting Platform! Register with us and start betting.
+             By opening an account with us, you agree to the following Terms and Conditions.</p>
+
+            <p className='font-bold pt-5 pb-2 underline underline-offset-5'>Account Terms And Conditions</p>
+            <p className='font-medium'>To use our services, simply register an account and deposit funds. Please read our Privacy Policy,
+             Rules and Regulations, Responsible Gambling, and FAQs including
+             the provisions with BAU88 Charges that are incorporated into the Terms and Conditions. Should there be any
+              inconsistencies between existing provisions, the Terms and Conditions will prevail.</p>
+
+          <p className='font-bold pt-5 pb-2 underline underline-offset-5'>Your Account</p>
+            <p className='font-medium'>By opening an account with us, the following representations are required: a)
+             You are at least 18 years of age. Gambling for minors is strictly prohibited and is punishable by law; b)
+              You are capable of taking responsibility of your actions, and can legally bind an agreement with us; c)
+               You agree to provide accurate information, including but not limited to date of birth and country of residence.
+                Furthermore, you agree to inform us of any changes in the foregoing information; d) You are the person whose
+                 details are provided in the registration process; e) You are acting as a principal and not as an agent on behalf 
+                 of a third party; f) You are not an undischarged bankrupt or in a voluntary arrangement with your creditors; g) 
+                 You are fully responsible in complying with your local, national, federal, state, or other laws regarding betting
+                  and gambling in connection with account registration, placing bets, and using any of our services; h) You will provide proper
+                   documentation and information upon request to identify your status, age, address, country of birth, and other such details
+                    deemed necessary in connection with opening and maintenance of your account. Furthermore, you agree that we verify the credibility
+                     of such information. It is your responsibility to update the foregoing information of any changes. You are responsible for the 
+                     security and confidentiality of your account. This includes your username and password that is used in logging into your account.
+                      You should frequently change your password and never disclose the same to any third party. You undertake to protect your 
+                      username and password and take full responsibility for any risks related to it. If another person manages to access your account,
+                       you are solely responsible for all their actions whether or not their access was authorized by you. Furthermore, you hereby
+                        indemnify us and hold us harmless against all costs, claims, expenses, and damages arising in connection with the use of your
+                         account by any third party. You will not sell, attempt to sell, or transfer the benefit of your account to any third party nor
+                          will you acquire or attempt to acquire an account
+                          opened and registered in the name of a third party. Manage your gambling activities fairly, securely, and responsibly.</p>
+
+          <p className='font-bold pt-5 pb-2 underline underline-offset-5'>Deposit And Withdrawal Of Funds</p>
+            <p className='font-medium'>To start betting, you need to deposit funds. You undertake that: a) 
+            All money deposited in your account is untainted with any illegality and does not originated from any illegal activity or source; b)
+             All payments made into your account are authorized and you will not attempt to reverse any payment made into your account or take any
+              action to such extent made by as third party in order to avoid any liability. c) You accept that all transactions may be checked to prevent
+               money laundering and any suspicious transactions will be reported to appropriate authorities. d) 
+            You agree that you are not entitled to any interest in your deposits and acknowledge that you shall not treat BABU88 as a financial institution.</p>
+
+            <p className='font-bold pt-5 pb-2 underline underline-offset-5'>Privacy</p>
+            <p className='font-medium'>All information sent to and received by us are processed under our Privacy
+             Policy. You authorize us, at any time, to use any means necessary to verify your identity and credentials
+              with any third-party providers of information. You agree that, from time to time, we may contact you 
+            to inquire about any promotional activity should you win a large sum of money, or place an unusually
+             successful bet. Should you wish to accept, we may use your name and/or image as necessary.
+             </p>
+
+           <p className='font-bold pt-5 pb-2 underline underline-offset-5'>Personal Data Security</p>
+            <p className='font-medium'>The company will ensure that your personal information is not disclosed to any third party.<br></br>
+            The company will also ensure the security of your personal information and ensure that your information is restricted to our company.</p>
+
+          <p className='font-bold pt-5 pb-2 underline underline-offset-5'>Central Wallet</p>
+            <p className='font-medium pb-5'>All deposited amount and bonus will be deposited into a single central wallet and bets can be placed.</p>
         </div>
     );
 };
