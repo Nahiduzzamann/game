fe63bf0de006e83a5fb4a82e9ed3cfbcc19c7d5b
Md Arif Hosain <arifhosain20284@gmail.com>
2024-01-10 14:23:29 +0600

    ok

--- frontEnd/src/pages/Home/ReferralsSection/ReferralsSection.jsx
+++ frontEnd/src/pages/Home/ReferralsSection/ReferralsSection.jsx
@@ -14,11 +14,11 @@ const ReferralsSection = () => {
                 <img className='w-full h-80 rounded-lg' src="https://img.freepik.com/premium-vector/futuristic-yellow-black-gaming-header-social-media-banner-template_136469-1827.jpg" alt="" />
                 <div className='hidden md:block'>
                 <p className='text-4xl font-bold pt-10 font-serif'>Bangladesh’s Trusted Online Casino and Cricket Exchange</p>
-                <p className='text-2xl font-bold leading-8 pt-3 pb-20 font-serif'>Babu88 is the premier online casino in India and Bangladesh, offering a variety of games for mobile and desktop users. 
+                <p className='text-2xl font-bold leading-8 pt-3 pb-20 font-serif'>40XBET is the premier online casino in India and Bangladesh, offering a variety of games for mobile and desktop users. 
                 Players can enjoy roulette, poker, baccarat, blackjack, and even cricket exchange betting options, with a chance to win real money online.
                  Our platform provides fast, seamless gameplay, and great bonuses for players. We prioritize safety and security, using advanced encryption
                   technology to protect your information, and our customer 
-                service is available 24/7. Join Babu88 today for the best online casino gaming and cricket exchange betting experience in Bangladesh.</p>
+                service is available 24/7. Join 40XBET today for the best online casino gaming and cricket exchange betting experience in Bangladesh.</p>
                 </div>
              </div>
         </div>
