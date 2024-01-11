c79e3e81c4c132125b7ab2c15eda7b5c4d59736b
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-01-11 12:50:55 +0600

    ok

--- frontEnd/src/components/PromotionsCard.jsx
+++ frontEnd/src/components/PromotionsCard.jsx
@@ -12,20 +12,20 @@ import {
 } from "@chakra-ui/react";
 import React from "react";
 
-const PromotionsCard = () => {
+const PromotionsCard = ({banner,title,description}) => {
   return (
-    <div>
+    <div className="shadow-lg ">
       <Card maxW="sm">
         <CardBody>
           <Image
-            src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-            alt="Green double couch with wooden legs"
+            src={banner}
+            alt="banner"
             borderRadius="lg"
           />
           <Stack mt="6" spacing="3">
-            <Heading size="md">Living room Sofa</Heading>
+            <Heading size="md">{title}</Heading>
             <Text>
-              This sofa is perfect for modern tropical spaces, baroque inspired
+          {description}
             </Text>
           </Stack>
         </CardBody>
@@ -33,10 +33,10 @@ const PromotionsCard = () => {
         <CardFooter>
           <ButtonGroup spacing="2">
             <Button variant="solid" colorScheme="blue">
-              Buy now
+             Details
             </Button>
-            <Button variant="ghost" colorScheme="blue">
-              Add to cart
+            <Button variant='outline' colorScheme="blue">
+             Apply Now
             </Button>
           </ButtonGroup>
         </CardFooter>
