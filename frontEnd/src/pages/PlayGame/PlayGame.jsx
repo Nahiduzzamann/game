40cabea4b32e77c4b0566b853927b8a0006ff83b
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-01-11 16:04:31 +0600

    ok

--- frontEnd/src/pages/PlayGame/PlayGame.jsx
+++ frontEnd/src/pages/PlayGame/PlayGame.jsx
@@ -1,14 +1,13 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import url from "../../module";
-import gateGame from "../../module/getGames";
+import gateGameById from "../../module/getGameById";
 
 const PlayGame = () => {
   const { id } = useParams();
   useEffect(() => {
     const games = async () => {
       try {
-        const res = await gateGame(url, id);
+        const res = await gateGameById(id);
         console.log(res.data);
       } catch (error) {
         // console.log(error);
