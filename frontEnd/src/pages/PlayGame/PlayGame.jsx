aa98c4df81b9b735c70fa42845b724c4ab3a77cc
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-01-11 17:01:17 +0600

    ok

--- frontEnd/src/pages/PlayGame/PlayGame.jsx
+++ frontEnd/src/pages/PlayGame/PlayGame.jsx
@@ -1,26 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import gateGameById from "../../module/getGameById";
 
 const PlayGame = () => {
   const { id } = useParams();
-//   console.log(id);
+  const [iframeUrl, setIframeUrl]= useState('')
+  //   console.log(id);
   useEffect(() => {
     const games = async () => {
       try {
         const res = await gateGameById(id);
-        console.log(res.data);
+        setIframeUrl(res.data.response);
       } catch (error) {
         console.log(error.response?.data?.error);
       }
     };
     games();
   }, [id]);
-  return (
-    <div>
-      <div>playinmg gamew</div>
-    </div>
-  );
+ 
+
+return (
+  <div className="w-full h-screen">
+    <iframe
+      title="GameIframe"
+      src={iframeUrl}
+      frameBorder="0"
+      width="100%"
+      height="100%"
+      allowFullScreen
+    ></iframe>
+  </div>
+);
 };
 
 export default PlayGame;
