c07ffa58ecc962ac81cfa5b1d6522a8ee1062adc
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-01-14 17:05:55 +0600

    login code done

--- controllers/userControler.ts
+++ controllers/userControler.ts
@@ -38,37 +38,35 @@ export const signUp = async (req: Request, res: Response) => {
   }
 };
 export const login = async (req: Request, res: Response) => {
-  try {
-    const { name, password, username, phone } = req.body;
-
-    // Validate request body
-    if (!name || !password || !username || !phone) {
-      return res.status(400).json({ message: 'All fields are required' });
+    try {
+      const { username, password } = req.body;
+  
+      // Validate request body
+      if (!username || !password) {
+        return res.status(400).json({ message: 'Username and password are required' });
+      }
+  
+      // Check if the user exists
+      const user = await Users.findOne({ username });
+  
+      if (!user) {
+        return res.status(401).json({ message: 'Invalid credentials' });
+      }
+  
+      // Ensure that user.password is non-null and non-undefined
+      const passwordMatch = await bcrypt.compare(password, user.password!);
+  
+      if (!passwordMatch) {
+        return res.status(401).json({ message: 'Invalid credentials' });
+      }
+  
+      // Generate a JSON Web Token (JWT)
+    //   const token = jwt.sign({ userId: user._id }, 'your-secret-key', { expiresIn: '1h' });
+      const token = 'token'
+  
+      res.status(200).json({ token });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
     }
-
-    // Check if the username is already taken
-    const existingUser = await Users.findOne({ username });
-    if (existingUser) {
-      return res.status(400).json({ message: 'Username is already taken' });
-    }
-
-    // Hash the password
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    // Create a new user with the hashed password
-    const newUser = new Users({
-      name,
-      password: hashedPassword,
-      username,
-      phone,
-    });
-
-    // Save the user to the database
-    await newUser.save();
-
-    res.status(201).json({ message: 'User registered successfully' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
-  }
-};
+  };
