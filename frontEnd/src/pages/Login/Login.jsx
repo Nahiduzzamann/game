86f23980892b4faebf2e9a71f4589de49ca15992
Md Arif Hosain <arifhosain20284@gmail.com>
2024-01-15 11:28:24 +0600

    ok

--- frontEnd/src/pages/Login/Login.jsx
+++ frontEnd/src/pages/Login/Login.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 const Login = () => {
     return (
         <div>
-            Login
+            <div class="grid grid-cols-4 justify-center">
+              <div class="col-start-2 col-span-4 ...">
+                <h1>Login </h1>
+              </div>
+            </div>
         </div>
     );
 };
