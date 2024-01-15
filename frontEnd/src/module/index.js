6b7ebb2e5835766ff48c852ef4d2550e2f2c911e
Sazzad Hossain <57707331+sazzad9911@users.noreply.github.com>
2024-01-15 09:12:50 +0600

    erer

--- frontEnd/src/module/index.js
+++ frontEnd/src/module/index.js
@@ -1,4 +1,4 @@
 
-//const  url="https://api.banglamartecommerce.com.bd/api"
-const  url="http://localhost:3100/api"
+const  url="https://api.banglamartecommerce.com.bd/api"
+//const  url="http://localhost:3100/api"
 export default url
