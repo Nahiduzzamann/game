b0c4ba470caaf98c37f359db38d450524d078b50
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-01-15 11:53:04 +0600

    user update page done

--- controllers/userControler.ts
+++ controllers/userControler.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
 import { Users } from '../connections/databaseConnection';
 const jwt = require('jsonwebtoken');
+
 export const signUp = async (req: Request, res: Response) => {
   try {
     const { name, password, username, phone } = req.body;
@@ -76,3 +77,46 @@ export const login = async (req: Request, res: Response) => {
     });
   }
 };
+
+
+
+export const updateUser = async (req: Request, res: Response) => {
+  try {
+    const { username,name, password, phone } = req.body;
+
+    // Validate request body
+    if(!name && !password && !phone) {
+      return res.status(400).json({ message: 'Invalid request' });
+    }
+
+    // Check if the user exists
+    const user = await Users.findOne({ username });
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    if (name) {
+      user.name = name;
+    }
+
+    if (password) {
+      // Hash the new password
+      const hashedPassword = await bcrypt.hash(password, 10);
+      user.password = hashedPassword;
+    }
+
+    if (phone) {
+      user.phone = phone;
+    }
+
+    // Save the updated user to the database
+    await user.save();
+
+    res.status(200).json({ message: 'User updated successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
--- routes/user.ts
+++ routes/user.ts
@@ -1,7 +1,8 @@
 import express,{Express} from 'express';
-import { login, signUp } from '../controllers/userControler';
+import { login, signUp, updateUser } from '../controllers/userControler';
 
 const user=express.Router()
 user.post("/signup",signUp)
 user.post("/login",login)
+user.put("/updateUser",updateUser)
 export default user
