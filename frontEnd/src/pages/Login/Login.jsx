b0b79ac7d7eb66abfd3264d20e5a9a624b6853a2
Md Arif Hosain <arifhosain20284@gmail.com>
2024-01-15 13:05:04 +0600

    ok

--- frontEnd/src/pages/Login/Login.jsx
+++ frontEnd/src/pages/Login/Login.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
+import {
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+  } from '@chakra-ui/react'
 
 const Login = () => {
     return (
-        <div>
-            <div class="grid grid-cols-4 justify-center">
-              <div class="col-start-2 col-span-4 ...">
-                <h1>Login </h1>
-              </div>
+        <div className='flex justify-center items-center h-screen bg-blue-50 p-25'>
+            <div className="w-96 p-6 shadow bg-white rounded-md border-4 border-indigo-500">
+                <p className='text-center text-3xl p-3 font-semibold'>Login Form</p>
+                <div className='border-b-2 pt-5 border-gray-400'></div>
+                <form action="">
+                <div className=" mt-3">
+                   <label for="username" className='block text-base mb-2'>Username:</label>
+                    <input type="text" className="w-full text-base py-1 px-1 border-2 border-gray-600 rounded" id="username" name="username" placeholder='Fill Up Hare'></input>
+                </div>
+
+                <div className=" mt-3">
+                   <label for="password" className='block text-base mb-2'>Password:</label>
+                    <input type="password" className="w-full text-base py-1 px-1 border-2 border-gray-600 rounded" id="password" name="password" placeholder='Fill Up Hare'></input>
+                    <div className='text-end text-indigo-800'><a href="facebook.com ">Forget Password?</a></div>
+                </div>
+
+                <div className="mt-4 text-center border-2 rounded p-1 bg-black text-yellow-100 hover:bg-red-400">
+                  <button className='text-center'>Login</button>
+                </div>
+               <div className='border-b-2 pt-5 border-red-400'></div>
+                <label for="password" className='block text-base mt-3'>Don't have an account?</label>
+                <div className="text-center border-2 rounded p-1 bg-yellow-500 text-black hover:bg-red-400">
+                  <button className='text-center'>Sing Up</button>
+                </div>
+
+                </form>
             </div>
         </div>
     );
