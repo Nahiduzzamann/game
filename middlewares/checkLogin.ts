1128b83289f59c371451a899a7c822f06dd8c69c
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-01-15 14:40:01 +0600

    ok

--- middlewares/checkLogin.ts
+++ middlewares/checkLogin.ts
@@ -8,18 +8,19 @@ export interface AuthenticatedRequest extends Request {
 export const authenticateToken = (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
   const token = req.header('Authorization');
   // console.log(token);
+  
 
   if (!token) {
     return res.status(401).json({ message: 'Unauthorized: Missing token' });
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET) as { username: string };
+    const decoded = jwt.verify(token.split(' ')[1], process.env.JWT_SECRET) as { username: string };
     req.username = decoded.username;
 
     next();
   } catch (error) {
-    console.error(error);
+    // console.error(error);
     res.status(401).json({ message: 'Unauthorized: Invalid token' });
   }
 };
