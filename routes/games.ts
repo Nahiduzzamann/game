bb661102ee411e8d05a9a45aa3433f511ed085f6
Sazzad Hossain <57707331+sazzad9911@users.noreply.github.com>
2024-01-15 17:24:23 +0600

    ert

--- routes/games.ts
+++ routes/games.ts
@@ -1,9 +1,11 @@
 import express,{Express} from 'express';
 import { callBack, getGameByCategory, getGameById, getGameCategories } from '../controllers/gameController';
+import multer from "multer"
+const upload = multer()
 
 const games=express.Router()
 games.get("/category",getGameCategories)
 games.get("/games/:gameIndex/:system",getGameByCategory)
 games.get("/games/:id/:user_id",getGameById)
-games.post("/callback",callBack)
+games.post("/callback",upload.none(),callBack)
 export default games
