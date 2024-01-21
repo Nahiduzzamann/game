060979740c833b804603f50e60f671d87c640e39
Md Arif Hosain <arifhosain20284@gmail.com>
2024-01-21 16:35:01 +0600

    ok

--- frontEnd/src/pages/Login/Login.jsx
+++ frontEnd/src/pages/Login/Login.jsx
@@ -4,6 +4,9 @@ import { AuthContext } from "./../../providers/AuthProvider";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import img from './login.png'
 import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { FaRegUserCircle } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
+
 const Login = () => {
   const { signIn, user, setUpdateUserState } = useContext(AuthContext);
   const [username, setUsername] = useState("");
@@ -60,50 +63,50 @@ const Login = () => {
     setShowPassword(!showPassword);
   };
   return (
-    <div className="flex justify-center items-center gap-10 h-screen bg-blue-50 p-25 flex-wrap">
+    <div className="flex justify-center items-center gap-10 h-screen bg-white p-25 flex-wrap">
       <div className=" hidden sm:block">
         <img className="object-fill" src={img}></img>
       </div>
-      <div className="w-96 p-6 shadow bg-white rounded-md border-4 border-blue-500">
+      <div className="w-96 p-6 shadow bg-gray-300 rounded-md">
         <p className="text-center text-3xl p-3 font-semibold">Login Form</p>
         <div className="border-b-2 pt-2 border-gray-400"></div>
 
         <form onSubmit={handleLogin}>
-          <div className="mt-5">
-            <label
-              htmlFor="username"
-              className="block text-base mb-2 font-semibold"
-            >
-              Username: <span className="text-red-500 font-bold">*</span>
-            </label>
-            <input
+          <div className="mt-16">
+            <div className="flex border border-blue-800 rounded-3xl">
+                  <div className="p-3">
+                    <FaRegUserCircle className="text-blue-800" />
+                  </div>
+            <input 
               type="text"
-              className="w-full text-base py-1 px-1 border-2 border-gray-600 rounded"
+              className="bg-gray-300"
               id="username"
               name="username"
-              placeholder="Fill Up Here"
+              placeholder="Username "
               value={username}
               onChange={(e) => setUsername(e.target.value)}
             />
+            </div>
+           
           </div>
 
-          <div className="mt-3">
-            <label
-              htmlFor="password"
-              className="block text-base mb-2 font-semibold"
-            >
-              Password: <span className="text-red-500 font-bold">*</span>
-            </label>
+          <div className="mt-10">
+            <div className="flex border border-blue-800 rounded-3xl">
+                  <div className="p-3">
+                  <FaLock className="text-blue-800" />
+                  </div>
+
             <div className="relative">
               <input
                 type={showPassword ? "text" : "password"}
-                className="w-full text-base py-1 px-1 border-2 border-gray-600 rounded"
+                className="bg-gray-300"
                 id="password"
                 name="password"
-                placeholder="Fill Up Here"
+                placeholder="Password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
+              </div>
               <div
                 className="absolute right-2 top-2 cursor-pointer"
                 onClick={togglePasswordVisibility}
@@ -111,6 +114,7 @@ const Login = () => {
                 {showPassword ? <FaEyeSlash /> : <FaEye />}
               </div>
             </div>
+
             <div className="text-end text-indigo-800 underline underline-offset-4">
               <Link to="/forgot-password">Forget Password?</Link>
             </div>
