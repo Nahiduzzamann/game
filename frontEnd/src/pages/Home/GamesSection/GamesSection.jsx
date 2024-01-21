b2b9ac6d45a622e2c6195d000d1832ecc8207475
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-01-21 17:38:24 +0600

    ok

--- frontEnd/src/pages/Home/GamesSection/GamesSection.jsx
+++ frontEnd/src/pages/Home/GamesSection/GamesSection.jsx
@@ -2,17 +2,18 @@ import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import url from "../../../module";
 import getCategory from "../../../module/getCategory";
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom";
+
 const GamesSection = () => {
   const [dataCategory, setDataCategory] = useState(null);
   const [dataSubCategory, setDataSubCategory] = useState(null);
   // console.log(dataSubCategory);
-  const navigate=useNavigate()
+  const navigate = useNavigate();
 
   useEffect(() => {
-    const cats =  () => {
+    const cats = () => {
       try {
-        const cat =  localStorage.getItem("category");
+        const cat = localStorage.getItem("category");
         setDataCategory(JSON.parse(cat));
       } catch (error) {
         console.error(error.message);
@@ -24,9 +25,9 @@ const GamesSection = () => {
   }, []);
 
   useEffect(() => {
-   if(dataCategory){
-    handleSubCategory(0)
-   }
+    if (dataCategory) {
+      handleSubCategory(0);
+    }
   }, [dataCategory]);
 
   const handleSubCategory = (index = 0) => {
@@ -37,20 +38,31 @@ const GamesSection = () => {
 
   return (
     <div className="px-6 py-3">
-      <Tabs isFitted variant="soft-rounded" colorScheme="blue">
-        <TabList mb="1em" className="flex gap-3 w-full  md:flex-wrap overflow-x-scroll bg-white rounded-md p-2">
-          {dataCategory &&
-            dataCategory?.map((data, i) => {
-              return (
-                <Tab
-                  onClick={() => handleSubCategory(i)}
-                  key={i}
-                  className="bg-gray-100 w-[150px] md:max-w-[150px] md:min-h-12 md:min-w-[220px] min-w-32 min-h-32 p-2 mb-2 md:mb-0"
-                >
-                  <h2 className="text-md text-black">{data?.title}</h2>
-                </Tab>
-              );
-            })}
+      <Tabs isFitted variant="" colorScheme="blue">
+        <TabList mb="1em" className="w-full bg-[#EBEBEB]  pt-4">
+          <div className="grid grid-cols-4 lg:gap-12 md:gap-10 gap-2 w-full">
+            {dataCategory &&
+              dataCategory?.map((data, i) => {
+                return (
+                  <Tab
+                    onClick={() => handleSubCategory(i)}
+                    key={i}
+                    style={{ padding: "0px", borderRadius:'10%' }}
+                    className="bg-[#D9D9D9]  w-full border-black shadow-md shadow-black relative "
+                  >
+                    <img
+                    style={{ padding: "0px", borderRadius:'10%' }}
+                      src={`./casino${i+1}.png`}
+                      alt="Your Image"
+                      className="object-cover object-center w-full h-full "
+                    />
+                    <div style={{ padding: "0px", borderRadius:'10%' }} className="flex items-end justify-center absolute w-full h-full  bg-gradient-to-b  from-[#fffcfc00] via-transparent to-[#000000]">
+                      <h2 className="md:text-2xl text-sm font-semibold text-white pb-2">{data?.title}</h2>
+                    </div>
+                  </Tab>
+                );
+              })}
+          </div>
         </TabList>
         <TabPanels className="bg-white rounded-md  ">
           {dataCategory &&
@@ -58,9 +70,13 @@ const GamesSection = () => {
               <TabPanel key={i}>
                 <div className="grid cursor-pointer md:grid-cols-4 xl:grid-cols-8 grid-cols-2">
                   {dataSubCategory?.map((data, j) => (
-                    <div onClick={()=>{
-                      navigate(`/games/${data.slag}/${i}`)
-                    }} key={j} className="p-2 flex gap-2 items-center bg-gray-700 hover:bg-blue-200 text-white m-2 shadow shadow-gray-200 rounded-md">
+                    <div
+                      onClick={() => {
+                        navigate(`/games/${data.slag}/${i}`);
+                      }}
+                      key={j}
+                      className="p-2 flex gap-2 items-center bg-gray-700 hover:bg-blue-200 text-white m-2 shadow shadow-gray-200 rounded-md"
+                    >
                       <img src={`${url}${data?.icon}`}></img>
                       <div className="text-lg">{data.title}</div>
                     </div>
