c89cadf8fca69a157cdbd65f3cafefe803ec48c4
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-01-21 13:01:55 +0600

    slider added

--- frontEnd/src/pages/Home/Slider/index.jsx
+++ frontEnd/src/pages/Home/Slider/index.jsx
@@ -1,47 +1,115 @@
 import React from "react";
-import { Slide } from "react-slideshow-image";
-import "react-slideshow-image/dist/styles.css";
-
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "./style.css";
+import img1 from './1.png'
+import img2 from './2.png'
+import img3 from './3.png'
+import img4 from './4.png'
+import img5 from './5.png'
+import img6 from './6.png'
+import { AiFillLeftCircle, AiFillRightCircle } from "react-icons/ai";
 export default function Slider() {
-  const spanStyle = {
-    padding: "20px",
-    background: "#efefef",
-    color: "#000000",
-  };
-
-  const divStyle = {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundSize: "cover",
-    height: "400px",
-    
-  };
-  const slideImages = [
-    {
-      url: "https://i.ibb.co/YjMKb2K/Untitled-design.png",
-      caption: "Slide 1",
-    },
-    {
-      url: "https://dl.memuplay.com/new_market/img/com.achievedreams.beach.sc0.2023-10-09-09-33-35_2x.jpg",
-      caption: "Slide 2",
-    },
-    {
-      url: "https://www.abestfashion.com/wp-content/uploads/2021/02/pexels-aidan-howe-4677402-scaled.jpg",
-      caption: "Slide 3",
-    },
+ 
+  const images = [
+    img1,
+    img2,
+    img3,
+    img4,
+    img5,
+    img6,
   ];
+  const rotateAnimationHandler = (props, state) => {
+    const transitionTime = props.transitionTime + "ms";
+    const transitionTimingFunction = "ease-in-out";
+    let slideStyle = {
+      display: "block",
+      minHeight: "100%",
+      transitionTimingFunction: transitionTimingFunction,
+      msTransitionTimingFunction: transitionTimingFunction,
+      MozTransitionTimingFunction: transitionTimingFunction,
+      WebkitTransitionTimingFunction: transitionTimingFunction,
+      OTransitionTimingFunction: transitionTimingFunction,
+      transform: `rotate(0)`,
+      position:
+        state.previousItem === state.selectedItem ? "relative" : "absolute",
+      inset: "0 0 0 0",
+      zIndex: state.previousItem === state.selectedItem ? "1" : "-2",
+      opacity: state.previousItem === state.selectedItem ? "1" : "0",
+      WebkitTransitionDuration: transitionTime,
+      MozTransitionDuration: transitionTime,
+      OTransitionDuration: transitionTime,
+      transitionDuration: transitionTime,
+      msTransitionDuration: transitionTime,
+    };
+    return {
+      slideStyle,
+      selectedStyle: {
+        ...slideStyle,
+        opacity: 1,
+        position: "relative",
+        zIndex: 2,
+        filter: `blur(0)`,
+      },
+      prevStyle: {
+        ...slideStyle,
+        transformOrigin: " 0 100%",
+        transform: `rotate(${
+          state.previousItem > state.selectedItem ? "-45deg" : "45deg"
+        })`,
+        opacity: "0",
+        filter: `blur( ${
+          state.previousItem === state.selectedItem ? "0px" : "5px"
+        })`,
+      },
+      
+    };
+  };
   return (
-    <div className="slide-container  mt-4">
-      <Slide indicators arrows={false}>
-        {slideImages.map((slideImage, index) => (
-          <div key={index}>
-            <div
-              style={{ ...divStyle, backgroundImage: `url(${slideImage.url})` }}
-            ></div>
+    <div className="box">
+      <Carousel
+        showIndicators
+        showStatus ={false}
+        showThumbs={false}
+        renderArrowNext={(clickHandler, hasNext) => {
+          return (
+            hasNext && (
+              <button className="nav_btn nav_btn_right" onClick={clickHandler}>
+                <AiFillRightCircle></AiFillRightCircle>
+              </button>
+            )
+          );
+        }}
+        renderArrowPrev={(clickHandler, hasNext) => {
+          return (
+            hasNext && (
+              <button onClick={clickHandler} className="nav_btn nav_btn_left">
+                <AiFillLeftCircle></AiFillLeftCircle>
+              </button>
+            )
+          );
+        }}
+        renderIndicator={(clickHandler, isSelected, index) => {
+          return (
+            <li
+           
+              onClick={clickHandler}
+              className={`ind ${isSelected ? "active" : ""} `}
+              key={index}
+              role="button"
+            />
+          );
+        }}
+        transitionTime={310}
+        animationHandler={rotateAnimationHandler}
+        swipeable={false}        
+      >
+        {images.map((URL, index) => (
+          <div key={index} className="slide">
+            <img alt="sample_file" src={URL} key={index} />
           </div>
         ))}
-      </Slide>
+      </Carousel>
     </div>
   );
 }
