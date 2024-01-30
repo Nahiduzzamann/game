3065f27f2e4b45fefc6de4e0b624f0b1f2c966fe
Sazzad Hossain <57707331+sazzad9911@users.noreply.github.com>
2024-01-30 15:33:54 +0600

    tyr

--- controllers/adminController.ts
+++ controllers/adminController.ts
@@ -5,40 +5,37 @@ import { StatusCodes } from "http-status-codes";
 
 interface MonthlyDepositTypes {
     month: string,
-    deposit: number
+    deposit: number,
+    year:number
 }
+interface MonthlyRevenueTypes {
+    month: string,
+    revenue: number,
+    year: number
+}
+
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 export const depositReport = async (req: Request, res: Response) => {
     try {
         let monthlyDeposit: MonthlyDepositTypes[] = []
-        const currentDate = new Date()
-        const deposit = await Deposit.find({ status: "ACCEPTED" }).sort({ date: -1 }) as DepositTypes[]
-        let totalDeposit = 0;
-        let currentMonth = deposit[0].date.getMonth()
+        let deposit = await Deposit.find({status:"ACCEPTED"}) as DepositTypes[] 
         let subTotal = 0;
-        deposit.map(d => {
-            if (d.date.getMonth() === currentMonth) {
-                //console.log(d.date.getMonth());
-
-                totalDeposit = totalDeposit + d.amount;
+        deposit.map(s => {
+            const isSame = monthlyDeposit.find(r => months[s.date.getMonth()] === r.month && s.date.getFullYear() === r.year)
+            if (isSame) {
+                monthlyDeposit[monthlyDeposit.indexOf(isSame)] = {
+                    month: months[s.date.getMonth()],
+                    year: s.date.getFullYear(),
+                    deposit: (isSame.deposit + s.amount)
+                }
             } else {
                 monthlyDeposit.push({
-                    month: months[currentMonth],
-                    deposit: totalDeposit
+                    month: months[s.date.getMonth()], 
+                    year: s.date.getFullYear(), deposit: s.amount
                 })
-                totalDeposit = 0;
-                currentMonth = d.date.getMonth();
-                totalDeposit = totalDeposit + d.amount;
             }
-            subTotal = subTotal + d.amount;
+            subTotal=(subTotal+s.amount)
         })
-        if (currentMonth === deposit[deposit.length - 1].date.getMonth()) {
-            monthlyDeposit.push({
-                month: months[currentMonth],
-                deposit: totalDeposit
-            })
-        }
-
         res.status(StatusCodes.OK).json({ monthlyDeposit, subTotal })
 
     } catch (error) {
@@ -47,32 +44,25 @@ export const depositReport = async (req: Request, res: Response) => {
 }
 export const revenueReport = async (req: Request, res: Response) => {
     try {
-        let monthlyHistory: MonthlyDepositTypes[] = []
-        let history = await History.find() as GameHistory[]
-        history = history.reverse()
-        let totalRevenue = 0;
-        let currentMonth = history[0].date.getMonth()
+        let monthlyHistory: MonthlyRevenueTypes[] = []
+        let history = await History.find() as GameHistory[] 
         let subTotal = 0;
-        history.map(d => {
-            if (d.date.getMonth() === currentMonth) {
-                totalRevenue = (totalRevenue + d.bet) - d.win;
+        history.map(s => {
+            const isSame = monthlyHistory.find(r => months[s.date.getMonth()] === r.month && s.date.getFullYear() === r.year)
+            if (isSame) {
+                monthlyHistory[monthlyHistory.indexOf(isSame)] = {
+                    month: months[s.date.getMonth()],
+                    year: s.date.getFullYear(),
+                    revenue: (isSame.revenue + s.bet) - s.win
+                }
             } else {
                 monthlyHistory.push({
-                    month: months[currentMonth],
-                    deposit: totalRevenue
+                    month: months[s.date.getMonth()], 
+                    year: s.date.getFullYear(), revenue: s.bet - s.win
                 })
-                totalRevenue = 0;
-                currentMonth = d.date.getMonth();
-                totalRevenue = (totalRevenue + d.bet) - d.win;
             }
-            subTotal = (subTotal + d.bet) - d.win;
+            subTotal=(subTotal+s.bet)-s.win;
         })
-        if (currentMonth === history[history.length - 1].date.getMonth()) {
-            monthlyHistory.push({
-                month: months[currentMonth],
-                deposit: totalRevenue
-            })
-        }
 
         res.status(StatusCodes.OK).json({ monthlyHistory, subTotal })
 
