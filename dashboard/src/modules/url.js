0d460726e4ab415a48dfee99666066874e1fdfc9
Sazzad Hossain <57707331+sazzad9911@users.noreply.github.com>
2024-03-04 12:40:50 +0600

    etg

--- dashboard/src/modules/url.js
+++ dashboard/src/modules/url.js
@@ -1,3 +1,3 @@
-//const  url="https://40xbet.online/api"
-const  url="http://localhost:3100/api"
+const  url="https://40xbet.online/api"
+//const  url="http://localhost:3100/api"
 export default url
