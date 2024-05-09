ccbe879aebd680178e9b00c2bfca00ed20ca6bd3
Sazzad Hossain <57707331+sazzad9911@users.noreply.github.com>
2024-05-09 12:47:56 +0600

    sfs

--- index.ts
+++ index.ts
@@ -16,11 +16,11 @@ import { getNotificationSocketAdmin, getNotificationUserSocket, getUnreadNotific
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 export const io = new SocketIOServer(server, {
   cors: {
-    origin: "http://localhost:3100",
+    origin: "http://localhost:5000",
     methods: ["GET", "POST","PUT"],
   },
 });
