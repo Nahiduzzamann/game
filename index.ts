8c6dfbb8d33f44de7b9aa0d533e1243f9c53ce82
Sazzad Hossain <57707331+sazzad9911@users.noreply.github.com>
2024-06-05 15:51:30 +0600

    hfg

--- index.ts
+++ index.ts
@@ -53,7 +53,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
-
+app.use("/wallets",express.static(path.join(__dirname, "data/wallets")))
 app.use(express.static(path.join(__dirname, "frontEnd/dist")));
 app.use(express.static(path.join(__dirname, "dashboard/dist")));
 app.use("/api", games)
@@ -63,6 +63,7 @@ app.use("/api/rewards",rewards)
 app.use("/api/icons",express.static(path.join(__dirname, "data/icons")))
 app.use("/api/images",express.static(path.join(__dirname, "data/images")))
 app.use("/api/data/images",express.static(path.join(__dirname, "data/images")))
+
 app.get("/deployment", (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, "deployment.php"))
 });
